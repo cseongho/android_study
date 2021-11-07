169199f76b2fdc3483ea9d30a03c068a1fe0755b
cseongho <dev-csh@naver.com>
2021-11-07 15:43:09 +0900

    Add fragment example project

---
+++ Example Project/Fragment_example/app/src/main/java/com/cookandroid/fragment_example/MainActivity.java
@@ -0,0 +1,65 @@
+package com.cookandroid.fragment_example;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button bt1,bt2,bt3;
+    FragmentManager fm;
+    FragmentTransaction tran;
+    Frag1 frag1;
+    Frag2 frag2;
+    Frag3 frag3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1 = (Button) findViewById(R.id.bt1);
+        bt2 = (Button) findViewById(R.id.bt2);
+        bt3 = (Button) findViewById(R.id.bt3);
+        bt1.setOnClickListener(this);
+        bt2.setOnClickListener(this);
+        bt3.setOnClickListener(this);
+        frag1 = new Frag1(); //프래그먼트 객채셍성
+        frag2 = new Frag2(); //프래그먼트 객채셍성
+        frag3 = new Frag3(); //프래그먼트 객채셍성
+        setFrag(0); //프래그먼트 교체
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.bt1:
+                setFrag(0);
+                break;
+            case R.id.bt2:
+                setFrag(1);
+                break;
+            case R.id.bt3:
+                setFrag(2);
+                break;
+        }
+    }
+    public void setFrag(int n){    //프래그먼트를 교체하는 작업을 하는 메소드를 만들었습니다
+        fm = getSupportFragmentManager();
+        tran = fm.beginTransaction();
+        switch (n){
+            case 0:
+                tran.replace(R.id.main_frame, frag1);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+            case 1:
+                tran.replace(R.id.main_frame, frag2);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+            case 2:
+                tran.replace(R.id.main_frame, frag3);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
+                tran.commit();
+                break;
+        }
+    }
+}
