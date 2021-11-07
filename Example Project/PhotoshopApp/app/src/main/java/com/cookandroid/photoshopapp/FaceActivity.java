11e0e7fcecdec21f24337297a868c964d49cc313
cseongho <dev-csh@naver.com>
2021-11-07 17:32:55 +0900

    add

---
+++ Example Project/PhotoshopApp/app/src/main/java/com/cookandroid/photoshopapp/FaceActivity.java
@@ -0,0 +1,104 @@
+package com.cookandroid.photoshopapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceContour;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.face.FaceLandmark;
+
+import java.util.List;
+
+public class FaceActivity extends Activity {
+
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_face);
+
+        // High-accuracy landmark detection and face classification
+        FaceDetectorOptions highAccuracyOpts =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                        .build();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.jwj);
+
+        InputImage image = InputImage.fromBitmap(bitmap);
+
+        FaceDetector detector = FaceDetection.getClient();
+
+        Task<List<Face>> result =
+                detector.process(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<Face>>() {
+                                    @Override
+                                    public void onSuccess(List<Face> faces) {
+                                        // Task completed successfully
+
+                                        for (Face face : faces) {
+                                            Rect bounds = face.getBoundingBox();
+                                            float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
+                                            float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
+
+                                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                                            // nose available):
+                                            FaceLandmark leftEar = face.getLandmark(FaceLandmark.LEFT_EAR);
+                                            if (leftEar != null) {
+                                                PointF leftEarPos = leftEar.getPosition();
+                                            }
+
+                                            // If contour detection was enabled:
+                                            List<PointF> leftEyeContour =
+                                                    face.getContour(FaceContour.LEFT_EYE).getPoints();
+                                            List<PointF> upperLipBottomContour =
+                                                    face.getContour(FaceContour.UPPER_LIP_BOTTOM).getPoints();
+
+                                            // If classification was enabled:
+                                            if (face.getSmilingProbability() != null) {
+                                                float smileProb = face.getSmilingProbability();
+                                            }
+                                            if (face.getRightEyeOpenProbability() != null) {
+                                                float rightEyeOpenProb = face.getRightEyeOpenProbability();
+                                            }
+
+                                            // If face tracking was enabled:
+                                            if (face.getTrackingId() != null) {
+                                                int id = face.getTrackingId();
+                                            }
+                                        }
+
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+
+
+
+    }
+}
